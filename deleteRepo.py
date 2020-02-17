eb25187486677ea21bfc285717d190dffd384270
Anchitaa25 <Anchitaa.25@gmail.com>
2020-02-17 06:48:36 -0500

    Update deleteRepo.py

--- deleteRepo.py
+++ deleteRepo.py
@@ -1,8 +1,8 @@
 import json
 import sys
 import requests
-
-r = requests.get('https://dev.azure.com/anchitaa95/deleteRepositoryPipeline/_apis/git/repositories/'+sys.argv[1]+'?api-version=5.1', auth = ('anchitaa95','vqluceje46tbuwz5xnjqhecpgebcg7cjakvbzfe43vg7m2e2zynq'))
+URL = 'https://dev.azure.com/anchitaa95/deleteRepositoryPipeline/_apis/git/repositories/'+sys.argv[1]+'?api-version=5.1'
+r = requests.get(url = URL, auth = ('anchitaa95','vqluceje46tbuwz5xnjqhecpgebcg7cjakvbzfe43vg7m2e2zynq'))
 data=r.json()
 print(data)
 json_string = json.dumps(data)
