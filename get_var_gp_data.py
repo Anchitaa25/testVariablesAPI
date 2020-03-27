571ecd759b094689123b7ce65d93577a0af9f0a9
Anchitaa25 <Anchitaa.25@gmail.com>
2020-03-27 14:33:10 +0530

    Update get_var_gp_data.py

--- get_var_gp_data.py
+++ get_var_gp_data.py
@@ -2,19 +2,21 @@ import json
 import sys
 import requests
 
+variableGroupName = sys.argv[1]
+user_name = sys.argv[2]
+auth_token = sys.argv[3]
+organisation_name = sys.argv[4]
+project_name = sys.argv[5]
 
-def deleteRepository():
-#  get_URL = 'https://dev.azure.com/'+Organization_Name+'/'+Project_Name+'/_apis/git/repositories/'+Repository_Name+'?api-version=5.1'
-  get_URL = 'https://dev.azure.com/guptashreya21/deleteRepo/_apis/distributedtask/variablegroups?groupName='+sys.argv[1]+'&api-version=5.0-preview.1'
-  get_ID_request = requests.get(url = get_URL , auth = ('guptashreya21','o6vnjl3lehhcv6brzatndur7u2jhcu6o5mbmsm2ia3put46vdy3q'))
+def getVariables():
+  get_URL = 'https://dev.azure.com/'+organisation_name+'/'+project_name+'/_apis/distributedtask/variablegroups?groupName='+variableGroupName+'&api-version=5.0-preview.1'
+  get_ID_request = requests.get(url = get_URL , auth = (user_name,auth_token))
   data = get_ID_request.json()
-  #print(data)
   variables = data["value"][0]["variables"]
   print(variables)
 
 try:
-
-  deleteRepository()
+  getVariables()
 except KeyError:
   print("URL Incorrect. Check values for Repository/Organization/Project Name")
 except ValueError:
