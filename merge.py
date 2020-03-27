fae928a117664e08ad9cb6ebbd0775967c9dc71e
Anchitaa25 <Anchitaa.25@gmail.com>
2020-03-27 13:09:10 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -8,19 +8,18 @@ variableValue = sys.argv[3]
 
 def updateVariableValue():
   get_URL = 'https://dev.azure.com/guptashreya21/deleteRepo/_apis/distributedtask/variablegroups?groupName='+variableGroupName+'&api-version=5.0-preview.1'
-  get_ID_request = requests.get(url = get_URL , auth = ('guptashreya21','o6vnjl3lehhcv6brzatndur7u2jhcu6o5mbmsm2ia3put46vdy3q'))
-  data = get_ID_request.json()
-  print(data)
-  variableGroupID = str(data["value"][0]["id"])
-  variables_json = data["value"][0]["variables"]
+  get_request = requests.get(url = get_URL , auth = ('guptashreya21','o6vnjl3lehhcv6brzatndur7u2jhcu6o5mbmsm2ia3put46vdy3q'))
+  get_data = get_request.json()
+  variableGroupID = str(get_data["value"][0]["id"])
+  variables_json = get_data["value"][0]["variables"]
   variables_json[variableName]["value"]= variableValue
-  del data["value"][0]["variables"]
-  data["value"][0]["variables"]= variables_json
-  id_content = {"id":variableGroupID,"type":"Vsts","name":variableGroupName,"variables":variables_json}
+  del get_data["value"][0]["variables"]
+  get_data["value"][0]["variables"]= variables_json
+  updated_json = {"id":variableGroupID,"type":"Vsts","name":variableGroupName,"variables":variables_json}
   header = {"Content-type": "application/json"}
   put_URL = 'https://dev.azure.com/guptashreya21/deleteRepo/_apis/distributedtask/variablegroups/'+variableGroupID+'?api-version=5.0-preview.1'
-  get_ID_request = requests.put(url = put_URL , auth = ('guptashreya21','o6vnjl3lehhcv6brzatndur7u2jhcu6o5mbmsm2ia3put46vdy3q'), data=json.dumps(id_content),headers =header)
-  data = get_ID_request.json()
+  put_request = requests.put(url = put_URL , auth = ('guptashreya21','o6vnjl3lehhcv6brzatndur7u2jhcu6o5mbmsm2ia3put46vdy3q'), data=json.dumps(updated_json),headers =header)
+  data = put_request.json()
   print(data)
 
 try:
