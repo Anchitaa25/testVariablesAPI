2c7fb3520ed73e47ea0bac339d4188fded6b91b8
Anchitaa25 <Anchitaa.25@gmail.com>
2020-03-27 15:28:36 +0530

    Update updateScript.py

--- updateScript.py
+++ updateScript.py
@@ -20,7 +20,8 @@ def updateVariableValue():
   del get_data["value"][0]["variables"]
   get_data["value"][0]["variables"]= variables_json
   updated_json = {"id":variableGroupID,"type":"Vsts","name":variableGroupName,"variables":variables_json}
- 
+  print(updated_json)
+  print(project_name)
   header = {"Content-type": "application/json"}
   put_URL = 'https://dev.azure.com/'+organisation_name+'/'+project_name+'/_apis/distributedtask/variablegroups/'+variableGroupID+'?api-version=5.0-preview.1'
   put_request = requests.put(url = put_URL , auth = (user_name,auth_token), data=json.dumps(updated_json),headers =header)
